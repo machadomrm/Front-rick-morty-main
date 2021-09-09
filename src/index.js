84d3752168887f235494973abc17dee92daa5777
machadomrm <machado.mrm@gmail.com>
2021-09-09 20:44:12 -0300

    Front-rick-morty-main

---
+++ src/index.js
@@ -0,0 +1,23 @@
+/*jshint esversion: 6 */
+/*jshint esversion: 8 */
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+import { App } from "./structure/app/App";
+
+import "./index.css";
+
+import "./styles/button.css";
+import "./styles/card.css";
+import "./styles/form.css";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
